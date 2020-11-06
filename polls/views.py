0a9b0bc9605880ff88f5647cb60b51c4b76356e6
余丰旭 <1425931231@qq.com>
2020-11-06 23:43:02 +0800

    完成投票应用

--- polls/views.py
+++ polls/views.py
@@ -2,22 +2,41 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render,get_object_or_404
 from .models import *
 from django.urls import reverse
+from django.views import generic    # 通用视图
 
-def index(request):
-    latest_question_list=Question.objects.order_by('-pub_date')[:5]
-    context={'latest_question_list':latest_question_list,
-             'name':'余丰旭'}
-    return render(request,'polls/index.html',context)
+# def index(request):
+#     latest_question_list=Question.objects.order_by('-pub_date')[:5]
+#     context={'latest_question_list':latest_question_list,
+#              'name':'余丰旭'}
+#     return render(request,'polls/index.html',context)
+#
+#     # question_id=34 由 urls中的<int:question_id> 匹配生成
+# def detail(request,question_id):
+#     # 从Question表中获取主键为url中<int:question_id>的一项
+#     question=get_object_or_404(Question,pk=question_id)
+#     return render(request,'polls/detail.html',{'question':question,'message':'用来测试的message'})
+#
+# def results(request,question_id):
+#     question=get_object_or_404(Question,pk=question_id)
+#     return render(request,'polls/results.html',{'question':question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    # 如果不写这一条，那么默认的是question_list
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+"""
+我们在这里使用两个通用视图： ListView 和 DetailView 。
+这两个视图分别抽象“显示一个对象列表”和“显示一个特定类型对象的详细信息页面”这两种概念。
+"""
+class DetailView(generic.DetailView):
+    model=Question
+    template_name = 'polls/detail.html'
 
-    # question_id=34 由 urls中的<int:question_id> 匹配生成
-def detail(request,question_id):
-    # 从Question表中获取主键为url中<int:question_id>的一项
-    question=get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/detail.html',{'question':question,'message':'用来测试的message'})
-
-def results(request,question_id):
-    question=get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/results.html',{'question':question})
+class ResultsView(generic.DetailView):
+    model=Question
+    template_name = 'polls/results.html'
 
 def vote(request,question_id):
     question=get_object_or_404(Question,pk=question_id)
